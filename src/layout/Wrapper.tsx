f2032691ec1c3213c2d5657812df174bbde89052
Priyanka K <priyanka.sony@gmail.com>
2024-09-23 19:38:01 +0530

    Remove Scroll Top

--- src/layout/Wrapper.tsx
+++ src/layout/Wrapper.tsx
@@ -3,7 +3,6 @@
 import { useEffect } from "react";
 import { ToastContainer } from "react-toastify";
 import { animationCreate } from "@/utils/utils";
-import ScrollToTop from "@/components/common/ScrollToTop";
 
 if (typeof window !== "undefined") {
     require("bootstrap/dist/js/bootstrap");
@@ -22,7 +21,6 @@ const Wrapper = ({ children }: any) => {
 
     return <>
         {children}
-        <ScrollToTop />
         <ToastContainer position="top-center" />
     </>;
 }
