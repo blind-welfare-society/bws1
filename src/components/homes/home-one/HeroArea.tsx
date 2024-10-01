f53d882b21c7c2c15205f98619b81aaca09b6068
blind-welfare-society <naveen@blindwelfaresociety.in>
2024-10-01 17:31:25 +0530

    Update HeroArea.tsx

--- src/components/homes/home-one/HeroArea.tsx
+++ src/components/homes/home-one/HeroArea.tsx
@@ -5,28 +5,12 @@ import axios from "@/lib/axios";
 import { useEffect, useState } from "react";
 
 const HeroArea = () => {
-   interface Banner {
-      id: number;
-      bannerImage: string;
-   }
-   const [banner, setBanner] = useState({ data: [] } as any);
-
-   useEffect(() => {
-      axios.get('/home-banners').then((res) => {
-         setBanner(res.data);
-      }).catch(err => console.error(err));
-   }, []);
-
-   const bannerList = banner.data;
+   
 
    return (
       <div className="hero-area">
          <div className="grid">
-            {bannerList.map((item: Banner, index: number) => (
-               <div className="grid__item" key={index}>
-                  <Image src={item.bannerImage} alt="Home Banner" priority={false} width={373} height={249} />
-               </div>
-            ))}
+            Hello
          </div>
       </div>
    )
