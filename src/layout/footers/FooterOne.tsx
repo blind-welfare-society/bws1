e42e7efaa053770c699b5fa5d31b1da02d7bd500
Priyanka K <priyanka.sony@gmail.com>
2024-10-04 14:13:58 +0530

    Changes in Footer

--- src/layout/footers/FooterOne.tsx
+++ src/layout/footers/FooterOne.tsx
@@ -1,84 +1,67 @@
+"use client"
 import Image, { StaticImageData } from "next/image"
-import Link from "next/link";
+import Link from "next/link"
 import SocialIcon from "@/components/common/SocialIcon";
 import footer_data from "@/data/footerData";
 
-import logo_1 from "@/assets/img/logos/logo-white.png";
 import donate from "@/assets/img/footer/donate-by.png";
 
-import footerGallery_1 from "@/assets/img/footer/gallery1.jpg";
-import footerGallery_2 from "@/assets/img/footer/gallery2.jpg";
-import footerGallery_3 from "@/assets/img/footer/gallery3.jpg";
-import footerGallery_4 from "@/assets/img/footer/gallery4.jpg";
-import footerGallery_5 from "@/assets/img/footer/gallery5.jpg";
-import footerGallery_6 from "@/assets/img/footer/gallery6.jpg";
+import footerGallery_1 from "@/assets/img/footer/gallery-two1.jpg";
+import footerGallery_2 from "@/assets/img/footer/gallery-two2.jpg";
+import footerGallery_3 from "@/assets/img/footer/gallery-two3.jpg";
+import footerGallery_4 from "@/assets/img/footer/gallery-two4.jpg";
+import footerGallery_5 from "@/assets/img/footer/gallery-two5.jpg";
+import footerGallery_6 from "@/assets/img/footer/gallery-two6.jpg";
 
-interface ContentData {
-   footer_about_text: JSX.Element;
-   gallery: StaticImageData[];
+interface ContentType {
+   copyright_text: string;
 }
 
-const footer_content: ContentData = {
-   footer_about_text: (<>Wimply dummy text of the priatype industry orem Ipsum has Maecenas quis eros at ante lacinia efficitur.</>),
-   gallery: [footerGallery_1, footerGallery_2, footerGallery_3, footerGallery_4, footerGallery_5, footerGallery_6],
+const footer_content: ContentType = {
+   copyright_text: "Copyright 2024 All Right Reserved",
 }
 
-const { footer_about_text, gallery } = footer_content;
+const {copyright_text } = footer_content
 
-const FooterOne = () => {
+const FooterTwo = () => {
    return (
-      <footer className="footer-area overlay text-white pt-120 bgs-cover" style={{ backgroundImage: `url(/assets/img/footer/footer-bg.jpg)` }}>
+      <footer className="footer-area footer-area--two text-white pt-70">
          <div className="container">
             <div className="row justify-content-between">
-               <div className="col-lg-3 col-md-6 col-sm-8">
+               <div className="col-xl-3 col-sm-6">
                   <div className="widget widget_about">
                      <div className="logo_footer mb-25">
-                        <Link href="/">
-                           <Image src={logo_1} alt="Logo" />
-                        </Link>
+                        <h5 className="">Blind Welfare Society</h5>
+                        <p><i className="fas fa-map-marker-alt"></i> Plot No. 2/16, Block WXYZ, Near M.N. Convent Public School, Kushak Road No. 2, Swaroop Nagar, New Delhi - 110042</p>
+                        <p><i className="fas fa-envelope"></i> <a href="mailto:info@blindwelfaresociety.in">info@blindwelfaresociety.in</a></p>
+                        <p><i className="fas fa-phone-alt"></i> <a href="tel:+91-11-25948803">+91-11-25948803</a></p>
+                        <p><i className="fas fa-mobile"></i> <a href="tel:+91-9968969932">+91-9968969932</a></p>
                      </div>
-                     <p>{footer_about_text}</p>
                      <div className="social-style-one pt-20">
                         <SocialIcon />
                      </div>
                   </div>
                </div>
 
-               {footer_data.filter((item) => item.page === "home_1").map((item) => (
-                  <div key={item.id} className="col-lg-2 col-md-3 col-sm-4 col-6">
+               {footer_data.filter((item) => item.page === "home_2").map((item) => (
+                  <div key={item.id} className="col-xl-2 col-sm-3 col-6">
                      <div className="widget widget_nav_menu">
                         <h5 className="widget-title">{item.widget_title}</h5>
-                        <ul>
-                           {item.footer_link.map((li, i) => (
-                              <li key={i}><Link href={li.link}>{li.link_title}</Link></li>
-                           ))}
-                        </ul>
+                        <ul>{item.footer_link.map((li, i) => (<li key={i}><Link href={li.link}>{li.link_title}</Link></li>))}</ul>
                      </div>
                   </div>
                ))}
 
-               <div className="col-lg-3 col-md-6 col-sm-8">
-                  <div className="widget widget_gallery">
-                     <h4 className="widget-title">Photo Gallery</h4>
-                     <div className="gallery-photos">
-                        {gallery.map((gallery, index) => (
-                           <a style={{cursor:"pointer"}} key={index} ><Image src={gallery} alt="Gallery" /></a>
-                        ))}
-                     </div>
-                  </div>
-               </div>
+              
             </div>
          </div>
          
-         <div className="footer-bottom mt-50">
+         <div className="footer-bottom mt-10">
             <div className="container">
                <div className="footer-bottom__inner">
-                  <div className="donate-by">
-                     <span>Donate by :</span>
-                     <Image src={donate} alt="Donate By" />
-                  </div>
+                 
                   <div className="copyright">
-                     <p>Copyright 2023 All Right Reserved</p>
+                     <p>{copyright_text}</p>
                   </div>
                </div>
             </div>
@@ -87,4 +70,4 @@ const FooterOne = () => {
    )
 }
 
-export default FooterOne
+export default FooterTwo
