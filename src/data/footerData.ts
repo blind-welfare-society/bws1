d96f990e589200a2e1e5eca4cb5cdf45e4594757
Priyanka K <priyanka.sony@gmail.com>
2024-10-04 14:31:53 +0530

    Changes in Footer

--- src/data/footerData.ts
+++ src/data/footerData.ts
@@ -35,13 +35,13 @@ const footer_data: DataType[] = [
       id:1,
       page:"home_2",
       widget_title: "About",
-      footer_link: [{ link: "#", link_title: "Home" }, { link: "#", link_title: "Donation" }, { link: "#", link_title: "About us" }, { link: "#", link_title: "Event" },]
+      footer_link: [{ link: "/", link_title: "Home" }, { link: "/our-projects", link_title: "Our Projects" }, { link: "#", link_title: "Donate" }, { link: "/news", link_title: "News & Events" },]
    },
    {
       id:2,
       page:"home_2",
       widget_title: "Quick links",
-      footer_link: [{ link: "#", link_title: "Causes" }, { link: "#", link_title: "About" }, { link: "#", link_title: "New campaign" }, { link: "#", link_title: "Site map" },]
+      footer_link: [{ link: "#", link_title: "Contact" }, { link: "/terms-conditions", link_title: "Terms & Conditions" }, { link: "/privacy-policy", link_title: "Privacy Policy" }, { link: "/donation-refund-policy", link_title: "Donation Refund Policy" }]
    },
 ]
 
