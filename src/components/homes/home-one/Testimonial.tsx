390344be1a490db50a4284d3e5882a23b3e30d11
Priyanka K <priyanka.sony@gmail.com>
2024-11-28 12:40:55 +0530

    Testimonial Homepage

--- src/components/homes/home-one/Testimonial.tsx
+++ src/components/homes/home-one/Testimonial.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image"
 import { useState, useEffect } from "react";
 import { fetchTestimonialsPosts, Post, PaginatedResponse } from '@/utils/apiTestimonials';
+import Slider from "react-slick"
 import Link from "next/link"
 
 const Testimonial = () => {
@@ -11,7 +12,7 @@ const Testimonial = () => {
    const [page, setPage] = useState(1);
    const [total, setTotal] = useState(0);
 
-   const limit = 3;
+   const limit = 6;
    useEffect(() => {
    const getPosts = async () => {
       try {
@@ -28,6 +29,30 @@ const Testimonial = () => {
    getPosts();
    }, [page]);
 
+   const settings = {
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: false,
+      autoplay: false,
+      fade: false,
+      dots: true,
+      // autoplaySpeed: 2000,
+      responsive: [
+         {
+            breakpoint: 1200,
+            settings: {
+               slidesToShow: 2,
+            }
+         },
+         {
+            breakpoint: 767,
+            settings: {
+               slidesToShow: 1,
+            }
+         }
+      ],
+   }
+
    return (
       <div className="testimonials-area-three py-120 rel z-1">
          <div className="container">
@@ -41,19 +66,21 @@ const Testimonial = () => {
                </div>
             </div>
             
-            <div className="row justify-content-center">
+            <Slider {...settings} className="course-page-slider row justify-content-center">
                {posts?.map((item: any) => (
                <div key={item.id} className="col-xl-4 col-md-6">
+                  <div className="mx-2">
                   <div className={`testimonial-item-three`}>
                      <div className="author"><Image src={item.image || ""} alt={item.name}  width={100} height={100} /></div>
                      <div className="text">{item.description}</div>
                      <h4 className="name">{item.name}</h4>
                      <span className="designation">{item.location}</span>
                   </div>
+                  </div>
                </div>
                ))}
-            </div>
-            <div className="text-center pt-30">
+            </Slider>
+            <div className="text-center pt-50">
                <Link href="/testimonials" className="cr-btn">View All</Link>
             </div>
          </div>
