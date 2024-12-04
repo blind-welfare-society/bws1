57f22ebda59f5f40a3433f32f8ba55bd796a8f66
Priyanka K <priyanka.sony@gmail.com>
2024-12-04 14:07:33 +0530

    Responsive Issue

--- src/components/common/Banner.tsx
+++ src/components/common/Banner.tsx
@@ -1,7 +1,7 @@
 
 const Banner = ({ image_url }: any) => {
    return (
-      <div className="page-banner-area bgs-cover page-banner1 text-white py-165 rpy-0 rmt-65" style={{ backgroundImage: `url(${image_url})` }}>
+      <div className="page-banner-area bgs-cover page-banner1 text-white py-165 rpy-60 rmt-65" style={{ backgroundImage: `url(${image_url})` }}>
          
       </div>
    )
