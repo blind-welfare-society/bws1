27fa888d982fc5fc56bebd0d25b9db6294b928a3
Priyanka K <priyanka.sony@gmail.com>
2024-12-09 16:14:50 +0530

    Change Donate FB Conversion Code

--- src/app/thank-you/[pageid]/page.tsx
+++ src/app/thank-you/[pageid]/page.tsx
@@ -1,5 +1,6 @@
 import ThankYou from "@/components/thank-you";
 import Wrapper from "@/layout/Wrapper";
+import Script from "next/script";
 
 export const metadata = {
    title: "Thank you | Blind Welfare Society",
@@ -9,6 +10,25 @@ const index = (params: any) => {
 
    return (
       <Wrapper>
+         <Script
+          id="google-conversion-inline-script"
+            dangerouslySetInnerHTML={{
+               __html: `
+                  gtag('event', 'conversion', {
+                        'send_to': 'AW-527459866/kZgGCJabreQBEJrMwfsB',
+                        'transaction_id': ''
+                    });
+               `,
+            }}
+            />
+            <Script
+            id="fb-conversion-inline-script"
+            dangerouslySetInnerHTML={{
+               __html: `
+               fbq('track', 'Donate');
+               `,
+            }}
+         />
          <ThankYou pageID={pageID} />
       </Wrapper>
    )
