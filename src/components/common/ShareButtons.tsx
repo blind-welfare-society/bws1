eca76ce95af78bab20c4152e5384d5bb988a4efb
Priyanka K <priyanka.sony@gmail.com>
2024-12-12 17:59:52 +0530

    Add Sharethis

--- src/components/common/ShareButtons.tsx
+++ src/components/common/ShareButtons.tsx
@@ -1,38 +1,35 @@
-import Script from 'next/script';
+declare global {
+  interface Window {
+    ShareThis?: any;
+  }
+}
+'use client';
+
 import { useEffect } from 'react';
 
 const ShareButtons = () => {
   useEffect(() => {
-    // Check if the ShareThis library is available and initialize the buttons
-    const initializeShareThis = () => {
-      if (window && (window as any).ShareThis) {
-        (window as any).ShareThis.load('inline-share-buttons', 0);
-        }
+    const loadShareThisScript = () => {
+      if (window?.ShareThis) {
+        window.ShareThis.reload();
+      } else {
+        const script = document.createElement('script');
+        script.src = 'https://platform-api.sharethis.com/js/sharethis.js#property=659d032eb492fb00132dce84&product=sop';
+        script.async = true;
+        script.onload = () => {
+          window.ShareThis?.reload();
+        };
+        document.body.appendChild(script);
+      }
     };
 
-    // Delay to ensure the script has loaded and the DOM is ready
-    const timer = setTimeout(() => {
-      initializeShareThis();
-    }, 500); // Small delay to ensure ShareThis script execution
-
-    return () => clearTimeout(timer); // Cleanup timeout if the component unmounts
-  }, []); // Runs once when the component is mounted
+    loadShareThisScript();
+  }, []);
 
   return (
-    <div>
-      <Script
-        src="https://platform-api.sharethis.com/js/sharethis.js#property=659d032eb492fb00132dce84&product=sop"
-        strategy="afterInteractive"
-        onLoad={() => {
-          if (window && (window as any).ShareThis) {
-            (window as any).ShareThis.load('inline-share-buttons', 0);
-          }
-        }}
-      />
-      <div className="shear mt-20 mb-30">
-        <h4>Spread The Word! Every Share Matters</h4>
-        <div className="sharethis-inline-share-buttons"></div>
-      </div>
+    <div className="share-buttons-container">
+      <h4>Spread The Word! Every Share Matters</h4>
+      <div className="sharethis-inline-share-buttons"></div>
     </div>
   );
 };
