7a1316f43c5d56c04074911061a5908363572a76
Priyanka K <priyanka.sony@gmail.com>
2024-12-17 20:21:55 +0530

    Change in menu

--- src/layout/headers/Menu/NavMenu.tsx
+++ src/layout/headers/Menu/NavMenu.tsx
@@ -1,8 +1,8 @@
 "use client";
-import Link from "next/link.js";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 import menu_data from "../../../data/MenuData";
-import React, { useState } from "react";
 
 const NavMenu = () => {
     const [navTitle, setNavTitle] = useState("");
@@ -16,52 +16,64 @@ const NavMenu = () => {
         return currentRoute === subMenuLink;
     };
 
-    //openMobileMenu
-    const openMobileMenu = (menu: any) => {
-        if (navTitle === menu) {
-            setNavTitle("");
-        } else {
-            setNavTitle(menu);
-        }
+    const openMenuOnHover = (menu: string) => {
+        setNavTitle(menu);
     };
 
-    const handleClick = (e:any) => { e.preventDefault();  console.log(''); };
+    const closeMenuOnBlur = () => {
+        setNavTitle("");
+    };
 
     return (
-        <ul className="navbar-nav menu-open text-lg-end">
+        <ul className="navbar-nav menu-open text-lg-end" role="menu">
             {menu_data.map((menu: any) => (
-                <li key={menu.id}  onClick={() => openMobileMenu(menu.title)} className={`${menu.has_dropdown ? "menu-item-has-children" : ""} 
-                    ${navTitle === menu.title ? "open" : ""} `}>
-                    <Link href={menu.link}
-                        {...(menu.link && isMenuItemActive(menu.link) ? { onClick: handleClick } : {})}
-                        className={`${(isMenuItemActive(menu.link) || (menu.sub_menus && menu.sub_menus.some((sub_m: any) => sub_m.link && isSubMenuItemActive(sub_m.link)))) ? "active" : ""}`}>
+                <li
+                    key={menu.id}
+                    className={`menu-item ${menu.has_dropdown ? "menu-item-has-children" : ""} ${
+                        navTitle === menu.title ? "open" : ""
+                    }`}
+                    role="button"
+                    aria-haspopup={menu.has_dropdown ? "true" : "false"}
+                    aria-expanded={menu.has_dropdown && navTitle === menu.title ? "true" : "false"}
+                    onMouseEnter={() => openMenuOnHover(menu.title)}
+                    onMouseLeave={closeMenuOnBlur}
+                    onFocus={() => openMenuOnHover(menu.title)} // For keyboard users
+                    onBlur={closeMenuOnBlur} // Close when focus leaves
+                >
+                    <Link
+                        href={menu.link}
+                        className={`menu-link ${
+                            navTitle === menu.title ? "active" : ""
+                        } ${(isMenuItemActive(menu.link) || (menu.sub_menus && menu.sub_menus.some((sub_m: any) => sub_m.link && isSubMenuItemActive(sub_m.link)))) ? "active" : ""}`}
+                    >
                         {menu.title}
                     </Link>
 
                     {menu.has_dropdown && (
-                        <>
-                            {menu.sub_menus && (
-                                <ul className="sub-menu">
-                                    {menu.sub_menus.map((sub_m: any, i: any) => (
-                                        <li key={i} onClick={() => openMobileMenu(menu.title)} className={`${navTitle === menu.title ? "open" : ""}`}>
-                                            <Link
-                                                href={sub_m.link}
-                                                className={
-                                                    sub_m.link && isSubMenuItemActive(sub_m.link) ? "active" : ""}>
-                                                {sub_m.title}
-                                            </Link>
-                                        </li>
-                                    ))}
-                                </ul>
-                            )}
-                        </>
+                        <ul
+                            className="sub-menu"
+                            role="menu"
+                            style={{ display: navTitle === menu.title ? "block" : "none" }}
+                        >
+                            {menu.sub_menus.map((sub_m: any, i: number) => (
+                                <li key={i} className="sub-menu-item" role="menuitem">
+                                    <Link
+                                        href={sub_m.link}
+                                        className={navTitle === menu.title ? "active" : ""}
+                                    >
+                                        {sub_m.title}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
                     )}
                 </li>
             ))}
-            <li className="donate-btn-mobile"><Link className="" href="/donate">Donate Now</Link></li>
+            <li className="donate-btn-mobile" role="menuitem">
+                <Link href="/donate">Donate Now</Link>
+            </li>
         </ul>
     );
 };
 
 export default NavMenu;
-
