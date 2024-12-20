0f3ac2dbe1dad2de8de26034b31f51f0c1faf065
Priyanka K <priyanka.sony@gmail.com>
2024-12-20 13:10:58 +0530

    accessible menu

--- src/layout/headers/Menu/NavMenu.tsx
+++ src/layout/headers/Menu/NavMenu.tsx
@@ -25,42 +25,62 @@ const NavMenu = () => {
         }
     };
 
+    const toggleSubMenu = (menuTitle: string) => {
+        setNavTitle((prev) => (prev === menuTitle ? "" : menuTitle));
+    };
+
     return (
         <ul className="navbar-nav menu-open text-lg-end">
             {menu_data.map((menu: any) => (
                 <li key={menu.id}
-                onClick={() => openMobileMenu(menu.title)}
-                    className={`${menu.has_dropdown ? "menu-item-has-children" : ""} 
+                    onClick={() => openMobileMenu(menu.title)}
+                    className={`nav-item ${menu.has_dropdown ? "menu-item-has-children" : ""} 
                     ${navTitle === menu.title ? "open" : ""} `}
+                    title={menu.title}
                 >
-                    <Link href={menu.link}
-                        className={`${(isMenuItemActive(menu.link) || (menu.sub_menus && menu.sub_menus.some((sub_m: any) => sub_m.link && isSubMenuItemActive(sub_m.link)))) ? "active" : ""}`}>
+                    <Link
+                        href={menu.link}
+                        role="button"
+                        aria-current={isMenuItemActive(menu.link) ? "page" : undefined}
+                        aria-expanded={menu.has_dropdown && navTitle === menu.title}
+                        aria-haspopup={menu.has_dropdown ? "true" : "false"}
+                        className={`${isMenuItemActive(menu.link) ? "active" : ""}`}
+                    >
                         {menu.title}
                     </Link>
 
                     {menu.has_dropdown && (
                         <>
                             {menu.sub_menus && (
-                                <ul className="sub-menu">
-                                    {menu.sub_menus.map((sub_m: any, i: any) => (
-                                        <li key={i} onClick={() => openMobileMenu(menu.title)} className={`${navTitle === menu.title ? "open" : ""}`}>
-                                            <Link
-                                                href={sub_m.link}
-                                                className={
-                                                    sub_m.link && isSubMenuItemActive(sub_m.link) ? "active" : ""}>
-                                                {sub_m.title}
-                                            </Link>
-                                        </li>
-                                    ))}
+                                <ul
+                                    className="sub-menu"
+                                    role="menu"
+                                    aria-label={`${menu.title} submenu`}
+                                    hidden={navTitle !== menu.title}
+                                >
+                                    {menu.sub_menus &&
+                                        menu.sub_menus.map((sub_m: any, i: number) => (
+                                            <li key={i} role="none">
+                                                <Link
+                                                    href={sub_m.link}
+                                                    role="menuitem"
+                                                    aria-current={isSubMenuItemActive(sub_m.link) ? "page" : undefined}
+                                                    className={isSubMenuItemActive(sub_m.link) ? "active" : ""}
+                                                >
+                                                    {sub_m.title}
+                                                </Link>
+                                            </li>
+                                        ))}
                                 </ul>
                             )}
                         </>
                     )}
                 </li>
             ))}
-            <li className="donate-btn-mobile"><Link className="" href="/donate">Donate Now</Link></li>
+            <li className="donate-btn-mobile" title="Donate Now"><Link role="menuitem" className="" href="/donate">Donate Now</Link></li>
         </ul>
     );
 };
 
 export default NavMenu;
+
