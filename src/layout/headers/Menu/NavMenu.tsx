05867086a432f2573f79f7f7b524e3e8bb7aa25f
Priyanka K <priyanka.sony@gmail.com>
2025-01-07 21:12:16 +0530

    change in menu

--- src/layout/headers/Menu/NavMenu.tsx
+++ src/layout/headers/Menu/NavMenu.tsx
@@ -34,28 +34,28 @@ const NavMenu = () => {
                 >
                     {menu.has_dropdown ? (
                         <a
-                        href={menu.link || "#"}
-                        role="button"
-                        aria-haspopup={menu.has_dropdown ? "true" : undefined}
-                        aria-expanded={menu.has_dropdown && expandedMenu === menu.title ? "true" : "false"}
-                        aria-controls={menu.has_dropdown ? `submenu-${menu.id}` : undefined}
-                        id={menu.has_dropdown ? `accessible-submenu-${menu.id}` : undefined}
-                        className={`nav-link ${isMenuItemActive(menu.link) ? "active" : ""}`}
-                        onClick={(e) => {
-                            if (menu.has_dropdown) {
-                                e.preventDefault();
-                                handleMenuToggle(menu.title);
-                            }
-                        }}
-                        onKeyDown={(e) => handleKeyPress(e, menu.title)}
-                        tabIndex={0}
+                            href={menu.link || "#"}
+                            role="button"
+                            aria-haspopup="true"
+                            aria-expanded={expandedMenu === menu.title ? "true" : "false"}
+                            aria-controls={menu.has_dropdown ? `submenu-${menu.id}` : undefined}
+                            id={menu.has_dropdown ? `accessible-submenu-${menu.id}` : undefined}
+                            className={`nav-link ${isMenuItemActive(menu.link) ? "active" : ""}`}
+                            onClick={(e) => {
+                                if (menu.has_dropdown) {
+                                    e.preventDefault();
+                                    handleMenuToggle(menu.title);
+                                }
+                            }}
+                            onKeyDown={(e) => handleKeyPress(e, menu.title)}
+                            tabIndex={0}
                         >
                             {menu.title}
                         </a>
                     ): (
                         <a
-                        href={menu.link || "#"}
-                        className={`nav-link ${isMenuItemActive(menu.link) ? "active" : ""}`}
+                            href={menu.link || "#"}
+                            className={`nav-link ${isMenuItemActive(menu.link) ? "active" : ""}`}
                         >
                             {menu.title}
                         </a>
@@ -64,8 +64,8 @@ const NavMenu = () => {
                         <div
                             id={`submenu-${menu.id}`}
                             className="sub-nav sub-menu"
-                            aria-expanded={expandedMenu == menu.title}
-                            aria-hidden={expandedMenu !== menu.title}
+                            aria-expanded={expandedMenu === menu.title ? "true" : "false"}
+                            aria-hidden={expandedMenu !== menu.title ? "true" : "false"}
                             aria-labelledby={`accessible-submenu-${menu.id}`}
                         >
                             <ul className="sub-nav-group">
